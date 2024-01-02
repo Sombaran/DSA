d7f5d1c4b3b67d696bf262103baebf19ef10f5be
Sombaran Gupta <sombaran.gupta@yahoo.com>
2024-01-02 21:56:23 +0530

    new assignment folder for array 240102A

--- bitManupulation/string_multiplication.cpp
+++ bitManupulation/string_multiplication.cpp
@@ -8,9 +8,8 @@ class playString {
 public:
 int convertStringToInterger (std::string receivedString) {
     int output=0;
-
     for (int i=0;i<receivedString.size();++i)
-        output = output * 10 + (receivedString[i]-'0');
+        output = output * 10 + receivedString[i]-'0'; // 
     std::cout << output << std::endl;
     return output;
 }
@@ -21,9 +20,9 @@ int convertStringToInterger (std::string receivedString) {
 int main () {
 playString stringObj;
 std::string s1,s2; std::cin>> s1 >> s2;
-int integer1 = stringObj.convertStringToInterger(s1);
-int integer2 = stringObj.convertStringToInterger(s2);
-std::cout << __func__ << integer1 << " " << s2 << std::endl;
+int integer1 = stringObj.convertStringToInterger(s1); // 13
+int integer2 = stringObj.convertStringToInterger(s2); // 13
+//std::cout << __func__ << integer1 << " " << s2 << std::endl;
 int answer = (integer1 * integer2);
 std::cout << "Final answer " << answer << std::endl;
 return (0);
