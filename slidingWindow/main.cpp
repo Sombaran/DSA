8185ec71eb91663a0dfb183940f359f95f67f20f
Sombaran Gupta <sombaran.gupta@yahoo.com>
2024-01-12 18:27:23 +0530

    240205C

--- slidingWindow/main.cpp
+++ slidingWindow/main.cpp
@@ -3,11 +3,12 @@
 
 int main(){
     std::shared_ptr<slidingWindow> fObj = std::make_shared<slidingWindow> ();
-    std::vector <int> vecOBj {1, 4, 2, 10, 2, 3, 1, 0, 20};
+    //std::vector <std::shared_ptr<int>> vecOBj = std::make_shared<std::vector<int>> ();
+    std::vector<int>vecObj{1, 4, 2, 10, 2, 3, 1, 0, 20};
     int k;
     std::cout << "Enter fixed window size =   ";
     std::cin >> k;
-    int result = fObj->max_sum_subarray(vecOBj, k);
+    int result = fObj->max_sum_subarray(vecObj, k);
     std::cout << result << std::endl;
     return (0);
 }
--- slidingWindow/subArray.cpp
+++ slidingWindow/subArray.cpp
@@ -5,23 +5,28 @@ https://medium.com/@coffee_addiction/window-sliding-technique-find-the-maximum-s
 #include "subArray.hpp"
 
 int slidingWindow::max_sum_subarray(std::vector<int>& received, int& k) {
-        std::cout << __func__ << std::endl;
-        if (k > received.size() or k < 0 or k == 0) {
-            std::cerr << "Window size needs to be checked \n";
-        }
-        int left, right = 0;
-        int currentSum = 0;
-        int maxSum = INT_MIN;
+    std::cout << __func__ << std::endl;
+    if (k > received.size() or k < 0 or k == 0) {
+        std::cerr << "Window size needs to be checked \n";
+    }
+    int left, right = 0;
+    int currentSum = 0;
+    int maxSum = INT_MIN;
 
-        while(right < received.size()) {
-            currentSum += received[right];
+    while(right < received.size()) {
+        currentSum += received[right];
 
-            if ((right-left+1) == k) {
-                maxSum = std::max(maxSum, currentSum);
-                currentSum -= received[left];
-                left++;
-            }
-            right++;
+        if ((right-left+1) == k) {
+            maxSum = std::max(maxSum, currentSum);
+            currentSum -= received[left];
+            left++;
         }
-        return maxSum;
+        right++;
+    }
+    return maxSum;
+}
+
+int slidingWindow::longest_sum_subarray(std::vector<int>& received, int& k) {
+
+    return ;
 }
